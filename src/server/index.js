7e6ba12062de4b5c628615cb3fd3ee3ae07e4a8e
Luis Vasquez <luis.guichol22@gmail.com>
2023-10-23 02:39:44 -0600

    Chat en tiempo real con nodejs socketio y turso

---
+++ src/server/index.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import logger from 'morgan';
+
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
+
+dotenv.config();
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const db = createClient({
+    url: process.env.DB_URL,
+    authToken: process.env.DB_TOKEN
+});
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
+    )
+`);
+
+
+io.on('connection', async (socket) => {
+    console.log('A new user has connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('chat message', async (msg) => {
+        let result;
+        const username = socket.handshake.auth.username ?? 'anonymous';
+        try {
+            result = await db.execute({
+                sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)", 
+                args: { msg, username }
+            });
+        } catch(error) {
+            console.log(error);
+            return;
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username);
+    });
+
+    if(!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content, user FROM messages WHERE id > :id',
+                args: { id: socket.handshake.auth.serverOffset ?? 0 }
+            })
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user);
+            })
+        } catch(error) {
+            console.error(error);
+            return;
+        }
+    }
+});
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/src/client/index.html');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
